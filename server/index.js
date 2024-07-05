34330e398cc21f5a093d9abc554275cf899721cc
Lyth Hammad <bigleo419@gmail.com>
2024-07-05 18:04:20 -0500

    Added authentication in the backend

--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js";
-import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import { createPost } from "./controllers/posts.js";
-import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
+// import userRoutes from "./routes/users.js";
+// import postRoutes from "./routes/posts.js";
+// import { createPost } from "./controllers/posts.js";
+// import { verifyToken } from "./middleware/auth.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
 
 /* Configuration */
 // This is to grab the file URL and the directory path
@@ -47,14 +47,33 @@ const storage = multer.diskStorage({
   });
   const upload = multer({ storage });
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /*MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
-mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-    }).catch((error) => console.log(`${error} did not connect`));
+// Assuming you have required Mongoose at the top of your file
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    // You can optionally process the error here, 
+    // such as exiting the application with a non-zero exit code
+    process.exit(1);
+  });
+
+// const PORT = process.env.PORT || 6001;
+// mongoose
+//     .connect(process.env.MONGO_URL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     })
+//     .then(() => {
+//         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+//     }).catch((error) => console.log(`${error} did not connect`));
